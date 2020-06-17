f02fb8ebf863813106d022d79c04307bf5f25f21
james wu <jwu_nyc@yahoo.com>
2020-06-17 04:24:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+  array.each_with_object([]) {|num,ara| ara<<num**2}
 end
+
+numbers=[1,2,3]
+def test(array)
+  ara=[]
+  array.each do |num|
+    ara << num**2
+  end
+  ara
+end
+
+def test2(array)
+  array.each_with_object([]) {|num,ara| ara<<num**2}
+end
+
+puts test2(numbers)
